6d4cce0e2a4c5ea06710d580027bff9726ea946a
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-11-27 13:04:04 -0500

    bugfix: set stan-stanc-path correctly

--- stan-mode.el
+++ stan-mode.el
@@ -90,8 +90,9 @@
   :group 'stan-mode)
 
 (defcustom stan-stanc-path
-  (if (member system-type '(windows-nt)) "stanc" "stanc.exe")
-  "stanc"
+  (if (member system-type '(windows-nt cygwin ms-dos))
+      "stanc.exe" 
+    "stanc")
   "Path to stanc executable"
   :type 'string
   :group 'stan-mode)
