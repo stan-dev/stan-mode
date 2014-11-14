3b631a21acef9507bbbab288438008776df71175
Jeff Arnold <jeffrey.arnold@gmail.com>
2014-11-14 04:01:03 -0800

    bump version

--- stan-snippets.el
+++ stan-snippets.el
@@ -5,9 +5,9 @@
 ;; Author: Jeffrey Arnold <jeffrey.arnold@gmail.com>
 ;; URL: http://github.com/stan-dev/stan-mode
 ;; Keywords: languages
-;; Version: 2.5.1
+;; Version: 2.5.2
 ;; Created: 2012-08-18
-;; Package-Requires: ((yasnippet "0.8.0") (stan-mode "2.5.1"))
+;; Package-Requires: ((yasnippet "0.8.0") (stan-mode "2.5.2"))
 
 ;; This file is not part of GNU Emacs.
 
