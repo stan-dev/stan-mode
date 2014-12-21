2d52a4980a0e9126b8a017dd4a704f47d57dc3e5
jrnold <jeffrey.arnold@gmail.com>
2014-12-21 09:24:15 -0800

    remove variable stan-stanc-bin
    It is not used anywhere

--- stan-mode.el
+++ stan-mode.el
@@ -94,18 +94,6 @@
   :type 'string
   :group 'stan-mode)
 
-(defcustom stan-stanc-path
-  (if (member system-type '(windows-nt cygwin ms-dos))
-      "stanc.exe"
-    "stanc")
-  "Path to stanc executable
-
-This can also be just the name of the stanc executable if it is on the PATH.
-"
-  :type 'string
-  :group 'stan-mode)
-
-
 
 ;;; cc-mode Language support
 
