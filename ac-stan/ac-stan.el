f10c6e85faf5ca69240cca39ca0d947598992f74
jrnold <jeffrey.arnold@gmail.com>
2015-07-20 10:57:36 -0700

    bump version numbers

--- ac-stan/ac-stan.el
+++ ac-stan/ac-stan.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Jeffrey Arnold <jeffrey.arnold@gmail.com>,
 ;; URL: http://github.com/stan-dev/stan-mode
 ;; Keywords: languages,completion
-;; Version: 1.1.0
+;; Version: 1.2.0
 ;; Created: 2014-12-18
 ;; Package-Requires: ((auto-complete "1.4.0") (stan-mode "5.0.0") (stan-snippets "4.0.0"))
 
