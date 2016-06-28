a9571366f212743eb919ecc87581f1524e683ef7
jrnold <jeffrey.arnold@gmail.com>
2016-06-27 22:56:35 -0700

    font lock warnings for variables ending in __

--- stan-mode/stan-mode.el
+++ stan-mode/stan-mode.el
@@ -427,6 +427,7 @@ does not accept the `word' option."
     ;; deprecated functions
     (,(stan-regexp-opt stan-deprecated-function-list) . font-lock-warning-face)
     (,(stan-regexp-opt stan-reserved-list) . font-lock-error-face)
+    ("\\w*__" . font-lock-error-face)
     ;; Variable declaration
     (,stan-var-decl-regexp 2 font-lock-variable-name-face)
     (,stan-func-decl-regexp 2 font-lock-variable-name-face)
