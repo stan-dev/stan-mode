45b8242611fe0437fcff48f5f4f7d8f0552531ac
jrnold <jeffrey.arnold@gmail.com>
2016-10-23 19:58:03 -0700

    bump version

--- ac-stan/ac-stan.el
+++ ac-stan/ac-stan.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Jeffrey Arnold <jeffrey.arnold@gmail.com>,
 ;; URL: http://github.com/stan-dev/stan-mode
 ;; Keywords: languages,completion
-;; Version: 9.1.0
+;; Version: 9.2.0
 ;; Created: 2014-12-18
-;; Package-Requires: ((auto-complete "1.4.0") (stan-mode "9.1.0") (stan-snippets "9.1.0"))
+;; Package-Requires: ((auto-complete "1.4.0") (stan-mode "9.2.0") (stan-snippets "9.2.0"))
 
 ;; This file is not part of GNU Emacs.
 
