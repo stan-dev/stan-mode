9eaaf042ead71d59ee8e5f81a5a0551132aa944c
kaz-yos <kazukiyoshida@mail.harvard.edu>
2019-07-26 22:40:48 -0400

    Update update-versions.sh file
    - Include files from new packages
    - Include python script with version string
    - Do not automatically run git
    - Do not tag automatically
    - Use gsed when sed is BSD sed

--- update-versions.sh
+++ update-versions.sh
@@ -5,17 +5,25 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 VERSION="$1"
-SED=sed
+
+# I need my sed -i command for in-place editing to work with both GNU sed and BSD/OSX sed
+# https://stackoverflow.com/questions/2320564/i-need-my-sed-i-command-for-in-place-editing-to-work-with-both-gnu-sed-and-bsd
+# Check sed for GNU sed in which --version works.
+# If not try to use gsed (GNU sed in Homebrew).
+# In macOS, install with brew install gnu-sed.
+if [ sed --version > /dev/null 2>&1 ]
+then
+    SED=sed
+else
+    SED=gsed
+fi
 
 function update_file_versions {
-    for f in stan-mode/stan-mode.el stan-snippets/stan-snippets.el ac-stan/ac-stan.el
+    for f in stan-mode/*.el stan-mode/create_stan_keywords.py stan-snippets/*.el ac-stan/*.el company-stan/*.el eldoc-stan/*.el flycheck-stan/*.el
     do
+        echo Updating the version string in $f
         $SED -i -e "s/^;; \+Version:\(.*\) *$/;; Version: $1/i" $f
-    done
-
-    # Update  dependencies in stan-snippets and ac-stan
-    for f in stan-snippets/stan-snippets.el ac-stan/ac-stan.el
-    do
+        echo Updating dependencies in $f
         $SED -i -e "s/(\(stan-mode\|stan-snippets\) \+\"\([0-9]\+\(\.[0-9]\+\)\+\)\")/(\1 \"$1\")/g" $f
     done
 }
@@ -24,8 +32,9 @@ git ls-files --other --error-unmatch . >/dev/null 2>&1; ec=$?
 if test "$ec" = 0
 then
     update_file_versions $VERSION
-    git commit -a -m "bump version"
-    git tag v$VERSION
+    # git commit -a -m "bump version"
+    # Tag on Github by creating a release
+    # git tag v$VERSION
     # git push
     # git push --tags
 elif test "$ec" = 1
@@ -35,4 +44,3 @@ then
 else
     echo "error from ls-files"
 fi
-
